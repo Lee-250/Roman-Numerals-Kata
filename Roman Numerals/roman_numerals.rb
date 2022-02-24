7c59c6e145974a5071b10675f8ddbf556801c869
Lee Broadhurst <lee.broadhurst@madetech.com>
2022-02-24 14:51:08 +0000

    Test and logic for IV

--- Roman Numerals/roman_numerals.rb
+++ Roman Numerals/roman_numerals.rb
@@ -4,7 +4,9 @@ class RomanNumerals
         if integer < 4
             return 'I' * integer
         end
-        
+
+        return 'IV'
+
     end
 
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -3,24 +3,29 @@ require_relative '../Roman Numerals/roman_numerals.rb'
 
 describe RomanNumerals do
     describe 'converter' do
+
         it 'Returns a string of I when provided with and integer of 1 ' do
         roman_numerals = described_class.new
 
         expect(roman_numerals.converter(1)).to eq('I')
         end
 
-
         it 'Returns a string of II when provided with an integer of 2' do
             roman_numerals = described_class.new
     
             expect(roman_numerals.converter(2)).to eq('II')
         end
-
         it 'Returns a string of III when provided with an integer of 3' do
             roman_numerals = described_class.new
 
             expect(roman_numerals.converter(3)).to eq('III')
         end
+        it 'Returns a string of IV when provided with an integer of 4' do
+            roman_numerals = described_class.new
+
+            expect(roman_numerals.converter(4)).to eq('IV')
+        end
+
 
 
     end
