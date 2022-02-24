9d307cdbd4a6417287982bf2f1d5b125afddf772
Lee Broadhurst <lee.broadhurst@madetech.com>
2022-02-24 16:29:28 +0000

    add tests and code for 10 - 14

--- Roman Numerals/roman_numerals.rb
+++ Roman Numerals/roman_numerals.rb
@@ -1,6 +1,9 @@
 class RomanNumerals
     def converter(integer)
+
+        values = { 4 => 'IV', 9 => 'IX'}
        
+
         # Deals with less than 4
         if integer < 4
             return 'I' * integer
@@ -17,15 +20,27 @@ class RomanNumerals
             return 'V' + 'I' * units
         end
 
+        # Deals with 9
         if integer == 9
             return 'IX'
         end
 
+        # Deals with 10 -> 13
+        if integer > 9 && integer < 14
+            units = integer % 10
+            return 'X' + 'I' * units
+        end
 
-       
-    
+        if integer == 14
+            return 'X' + values[4]
+        end
+      
 
     end
 
 end
+
+numerals = RomanNumerals.new
+
+puts numerals.converter(2)
 
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -54,6 +54,30 @@ describe RomanNumerals do
             expect(roman_numerals.converter(9)).to eq('IX')
         end
 
+        it 'Returns a string of X when provided with an integer of 10' do
+            roman_numerals = described_class.new
+
+            expect(roman_numerals.converter(10)).to eq('X')
+        end
+
+        it 'Can convert integers between 11 -13' do
+            roman_numerals = described_class.new
+
+            expect(roman_numerals.converter(11)).to eq('XI')
+            expect(roman_numerals.converter(12)).to eq('XII')
+            expect(roman_numerals.converter(13)).to eq('XIII')
+        end
+
+        it 'Returns a string of XIV when provided with an integer of 14' do
+            roman_numerals = described_class.new
+
+            expect(roman_numerals.converter(14)).to eq('XIV')
+        end
+
+
+
+
+
 
 
     end
